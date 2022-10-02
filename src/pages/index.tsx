f4cc857a728159a0a75f831137e38eb53e6feb55
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-10-02 18:25:53 +0700

    Initialize project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Home } from "@modules/home";
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default HomePage;
